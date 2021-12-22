3bb74833890745afe36cbd7ad006242d2b6f4903
KeremAydın <77073029+KeremAydin98@users.noreply.github.com>
2021-12-22 16:14:37 +0300

    Update regression_main.py

--- regression/regression_main.py
+++ regression/regression_main.py
@@ -8,7 +8,9 @@ import numpy as np
 
 pro = preprocess()
 
-train_features,train_labels,test_features,test_labels = pro.importing_data()
+train_df,test_df = pro.importing_data()
+
+train_features,train_labels,test_features,test_labels = pro.remove_uncorrelated(train_df,test_df)
 
 train_features_norm = pro.normalization(train_features)
 train_labels_norm = pro.normalization(train_labels)
