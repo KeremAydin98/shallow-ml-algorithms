006afc5d45f971ed1b4a089e09eed8c343fac14f
KeremAydın <77073029+KeremAydin98@users.noreply.github.com>
2021-12-22 16:09:00 +0300

    Update preprocessing.py

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -10,24 +10,34 @@ class preprocess():
         test_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_test.csv")
         train_df = train_df.reindex(np.random.permutation(train_df.index))  # shuffle the training set
 
-        print(train_df.corr()["median_house_value"])
-
         train_df["bedroom_per_rooms"] = train_df["total_bedrooms"] / train_df["total_rooms"]
         test_df["bedroom_per_rooms"] = test_df["total_bedrooms"] / test_df["total_rooms"]
-
-        print(train_df.corr()["median_house_value"])
 
         threshold = 265000
         train_df["median_high"] = (train_df["median_house_value"] > threshold).astype(float)
         test_df["median_high"] = (test_df["median_house_value"] > threshold).astype(float)
+        
+        train_df = train_df.drop("median_house_value",axis=1)
+        test_df = test_df.drop("median_house_value",axis=1)
 
-        train_df = train_df.drop("total_bedrooms", axis=1)
-        train_df = train_df.drop("population", axis=1)
-        train_df = train_df.drop("median_house_value", axis=1)
+        return train_df, test_df
 
-        test_df = test_df.drop("total_bedrooms", axis=1)
-        test_df = test_df.drop("population", axis=1)
-        test_df = test_df.drop("median_house_value", axis=1)
+    def remove_uncorrelated(self,train_df,test_df):
+
+        corr = train_df.corr()["median_high"]
+
+        abs_corr = abs(corr)
+
+        sorted_corr = abs_corr.sort_values()
+
+        low_correlations = sorted_corr[sorted_corr < 0.05]
+
+        low_correlations_indexes = list(low_correlations.index)
+
+        for drop in low_correlations_indexes:
+            train_df = train_df.drop(drop, axis=1)
+            test_df = test_df.drop(drop, axis=1)
+
 
         train_features = train_df.drop("median_high", axis=1)
         train_labels = train_df["median_high"]
