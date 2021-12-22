4157df7a1d62ff6dd7309798821330bd5bd83fe5
KeremAydın <77073029+KeremAydin98@users.noreply.github.com>
2021-12-22 16:14:10 +0300

    Update preprocessing.py

--- regression/preprocessing.py
+++ regression/preprocessing.py
@@ -10,19 +10,30 @@ class preprocess():
         test_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_test.csv")
         train_df = train_df.reindex(np.random.permutation(train_df.index))  # shuffle the training set
 
+        return train_df,test_df
+
+    def remove_uncorrelated(self,train_df,test_df):
+
         print(train_df.corr()["median_house_value"])
 
         # total bedrooms do not affect the prediction a lot so we are going to create a new column called bedroom per room
         train_df["bedroom_per_rooms"] = train_df["total_bedrooms"] / train_df["total_rooms"]
         test_df["bedroom_per_rooms"] = test_df["total_bedrooms"] / test_df["total_rooms"]
 
-        print(train_df.corr()["median_house_value"])
+        # Therefore we will drop the columns which have low correlation values
+        corr = train_df.corr()["median_high"]
 
-        # total bedrooms and population do not affect either therefore we can drop the columns
-        train_df = train_df.drop("total_bedrooms", axis=1)
-        train_df = train_df.drop("population", axis=1)
-        test_df = test_df.drop("total_bedrooms", axis=1)
-        test_df = test_df.drop("population", axis=1)
+        abs_corr = abs(corr)
+
+        sorted_corr = abs_corr.sort_values()
+
+        low_correlations = sorted_corr[sorted_corr < 0.05]
+
+        low_correlations_indexes = list(low_correlations.index)
+
+        for drop in low_correlations_indexes:
+            train_df = train_df.drop(drop, axis=1)
+            test_df = test_df.drop(drop, axis=1)
 
         train_features = train_df.drop("median_house_value", axis=1)
         train_labels = train_df["median_house_value"]
@@ -33,5 +44,7 @@ class preprocess():
 
     def normalization(self,data):
         data_norm = (data - data.mean()) / data.std()
+
+        return data_norm
 
         return data_norm
